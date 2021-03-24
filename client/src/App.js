d2d2c68dff1a94da78f15102ae2a835a34b6770f
danieleliyho <77384369+danieleliyho@users.noreply.github.com>
2021-03-24 02:04:47 +0200

    show less&more works

--- client/src/App.js
+++ client/src/App.js
@@ -6,36 +6,44 @@ function App() {
   const [tickets, settickets] = useState(null)
   const [seeMoreAndLess, setseeMoreAndLess] = useState('show more')
   const [seeMoreButtonClass, setseeMoreButtonClass] = useState('ticketOpen')
-  function connectReview(){
-    if(seeMoreAndLess === 'show more'){
-      alert(11)
-      setseeMoreAndLess('show less')
-      setseeMoreButtonClass('ticketClose')
-    }else{
-      alert(121)
-      setseeMoreAndLess('show more')
-      setseeMoreButtonClass('ticketOpen')
-    }
-  }
-  useEffect(() => {
 
+  //  const changeValue = (e) => {
+  //   const newValue = e.target.value;
+  //   setValue(newValue)
+  // }
+  useEffect(() => {
+    const connectReview = (e) =>{
+      if(e.target.innerText === 'show more'){
+        alert(11)
+        e.target.innerText = 'show less'
+        e.target.parentElement.className="ticketClosed"
+        // setseeMoreAndLess('show less')
+      // return  setseeMoreButtonClass('ticketClose')
+      }else{
+        alert(121)
+        e.target.innerText = 'show more'
+        e.target.parentElement.className="ticketOpen"
+      }
+    }
     axios.get('/api/tickets')
     .then(({data})=>{
-     let x = data.map((x)=>{
+      let ticketData = data;
+     let x = ticketData.map((x,i)=>{
       let dateFormat = new Date(x.creationTime).toLocaleString();
         return(
-          <div className={seeMoreButtonClass}>
+          <div className={seeMoreButtonClass} key={i}>
           <h3 className="title"> {x.title}</h3>
           <p className="content"> {x.content}</p>
           <p className="done"> {x.done}</p>
-          <div onClick={() =>connectReview()}>{seeMoreAndLess}</div>
+          <div key={i} onClick={(e) =>(connectReview(e))}>{seeMoreAndLess}</div>
           <span className="bottomline userEmail"> by {x.userEmail}  </span>
           <span className="bottomline creationTime"> {dateFormat}</span>
+          <p className="done"> {x.labels.map((c)=>{return c})}</p>
           </div> 
      )})
       settickets(x)
     })
-  }, [seeMoreAndLess,seeMoreButtonClass])
+  })
 
 console.log(tickets)
   return (
